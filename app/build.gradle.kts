1a4d529930a85fce2ded772de18b0efdc8b8cb01
eatenlamp <akpasovrinat@gmail.com>
2026-07-30 14:10:27 +0700

    Fix keystore path: go up one level

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file("netspoofer.jks")
+            storeFile = file("../netspoofer.jks")
             storePassword = System.getenv("KEYSTORE_PASSWORD")
             keyAlias = System.getenv("KEY_ALIAS")
             keyPassword = System.getenv("KEY_PASSWORD")
